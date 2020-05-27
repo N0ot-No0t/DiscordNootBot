00242cadd86a4fb4bc38ac182289c72728dcfcc3
N0ot-No0t <sustaccosmin544@hotmail.ca>
2020-05-27 03:40:55 -0400

    Added mark feature, where the user can trigger the bot on specific users whenever they join a voice chat.

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ var isReady = true;
 
 var specialRole = "NootPass"
 
+var markArray = new Array();
+
 //note: noot3 is bass boost and noot8 is glitch noot
 
 bot.on("ready", () => {
@@ -34,45 +36,39 @@ bot.on("message", async msg => {
 
         case "noot":
 
+            console.log("******arg1: " + args[1]);
+            //console.log(msg.mentions.members.first());
 
             if (args[1] == "glitch" && msg.member.roles.cache.some(role => role.name === specialRole)) {
 
-                playSpecifiAudio(10, msg);
+                playAudio(10, msg);
 
             } else if (args[1] == "loud" && msg.member.roles.cache.some(role => role.name === specialRole)) {
 
-                playSpecifiAudio(11, msg);
+                playAudio(11, msg);
 
-            } else {
+            } else if (args.length > 1 && args[1].startsWith('<@') && args[1].endsWith('>')) {
 
-
-                if (msg.member.voice.channel) {
-
-                    var voiceChannel = msg.member.voice.channel;
-
-                    await voiceChannel.join().then(connection => {
-
-                        var dispatcher = connection.play("./assets/audio/noot" + getRandomAudioID() + ".mp3");
-
-                        //console.log("Playing noot" + getRandomAudioID());
-
-                        dispatcher.on('finish', () => {
-                            console.log("Finished playing.")
-                            //connection.disconnect();
-                            voiceChannel.leave();
-                            //dispatcher.pause();
-                        });
-
-                        console.log("Left Channel");
-
-                    }).catch(err => console.log(err));
-
+                //console.log(getUserFromMention(args[1]).id);
+                if (args[2] !== undefined) {
+                    markUser(getUserFromMention(args[1]).id, args[2]);
                 } else {
-                    msg.reply('NOOT NOOT!');
+                    markUser(getUserFromMention(args[1]).id, "random");
                 }
 
-                isReady = true;
+                console.log("******markArray length: " + markArray.length);
 
+                for (var i = 0; i < markArray.length; i++) {
+                    console.log(markArray[i]);
+                }
+
+
+            } else if (args.length === 1) {
+
+                playAudio(getRandomAudioID(), msg);
+
+            } else {
+                msg.reply('Noot?');
             }
 
     }
@@ -88,6 +84,55 @@ bot.on("message", async msg => {
 // });
 
 
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;
+    //console.log("newUserChannel: " + newUserChannel);
+    //console.log("oldUserChannel: " + oldUserChannel);
+
+    if ((oldUserChannel === undefined && newUserChannel !== undefined) || (oldUserChannel === null && newUserChannel !== undefined)) {
+
+        // User Joins a voice channel
+
+        console.log("user joined VC");
+
+        //console.log("new member channel ID " + newUserChannel);
+
+        for (var i = 0; i < markArray.length; i++) {
+
+            if (markArray[i].uID === newMember.id) {
+
+                console.log("markArray[i].command: " + markArray[i].command);
+
+                switch (markArray[i].command) {
+
+                    case "loud": playAudioNoMsg(11, newUserChannel);
+                        break;
+                    case "glitch": playAudioNoMsg(10, newUserChannel);
+                        break;
+                    case "random": playAudioNoMsg(getRandomAudioID(), newUserChannel);
+                        break;
+
+                }
+
+                markArray.splice(i, 1)
+                break;
+
+            }
+
+        }
+
+
+    } else if (newUserChannel === null) {
+
+        // User leaves a voice channel
+
+        console.log("user left VC");
+
+    }
+});
+
+
 function getRandomAudioID() {
 
     var index = (Math.floor(Math.random() * 8) + 1);
@@ -97,11 +142,13 @@ function getRandomAudioID() {
     return index;
 }
 
-async function playSpecifiAudio(index, msg) {
+async function playAudio(index, msg) {
 
     if (msg.member.voice.channel) {
 
         var voiceChannel = msg.member.voice.channel;
+
+        console.log("msg.member.voice.channel; " + msg.member.voice.channel);
 
         await voiceChannel.join().then(connection => {
 
@@ -123,4 +170,51 @@ async function playSpecifiAudio(index, msg) {
     }
 
     isReady = true;
+}
+
+async function playAudioNoMsg(index, channel) {
+
+
+    let voiceChannel = bot.channels.cache.get(channel);
+
+
+    await voiceChannel.join().then(connection => {
+
+        var dispatcher = connection.play("./assets/audio/noot" + index + ".mp3");
+
+        dispatcher.on('finish', () => {
+            console.log("Finished playing.")
+            //connection.disconnect();
+            voiceChannel.leave();
+            //dispatcher.pause();
+        });
+
+        console.log("Left Channel");
+
+    }).catch(err => console.log(err));
+
+}
+
+//from https://discordjs.guide/miscellaneous/parsing-mention-arguments.html#implementation
+function getUserFromMention(mention) {
+    if (!mention) return;
+
+    if (mention.startsWith('<@') && mention.endsWith('>')) {
+        mention = mention.slice(2, -1);
+
+        if (mention.startsWith('!')) {
+            mention = mention.slice(1);
+        }
+
+        return bot.users.cache.get(mention);
+    }
+}
+
+async function markUser(userID, commandArg) {
+
+    var mark = new Object();
+    mark.uID = userID;
+    mark.command = commandArg;
+
+    markArray.push(mark);
 }
